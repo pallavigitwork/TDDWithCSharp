e44a97c62e9eb3300e42fd72cad7b65d8c5b5f2e
Pallavai <info@5elementslearning.dev>
2021-03-24 05:19:12 +0530

    TDDwithFizzBuzz

--- FizzBuzz/FizzBuzzClass.cs
+++ FizzBuzz/FizzBuzzClass.cs
@@ -2,54 +2,75 @@
 
 namespace FizzBuzz
 {
-    
+
     public class FizzBuzzClass
     {
-        public String execute(int[] data)
-        {
-            String str = "";
-            String delim = ",";
-            for (int i = 0; i < data.Length; i++)
-            {
-                if (i == 0)
-                {
-                    str = process(data[i]);
-                }
-                else { 
-                    str = str + delim + process(data[i]);
-                }
-            }
-            
-            return str;
+          public String Execute(int[] data)
+          {
+              String str = "";
+              String delim = ",";
+              for (int i = 0; i < data.Length; i++)
+              {
+                  if (i == 0)
+                  {
+                      str = Process(data[i]);
+                  }
+                  else { 
+                      str = str + delim + Process(data[i]);
+                  }
+              }
 
-        }
+              return str;
 
-        public string process(int num)
+          }
+        
+
+          public string Process(int num)
+          {
+              if (dividedBy3(num) && dividedBy5(num))
+              {
+                  return "FizzBuzz";
+              }
+              if (dividedBy3(num))
+              {
+                  return "Fizz";
+              }
+              if (dividedBy5(num))
+              {
+                  return "Buzz";
+              }
+
+              return num.ToString();
+          }
+
+          public Boolean dividedBy3(int num)
+          {
+              return num % 3 == 0;
+          }
+          public Boolean dividedBy5(int num)
+          {
+              return num % 5 == 0;
+          }
+
+      
+
+        /*public String Deliver(int num)
         {
-            if (dividedBy3(num) && dividedBy5(num))
+            if ((num % 3) == 0 && (num %5==0))
             {
                 return "FizzBuzz";
             }
-            if (dividedBy3(num))
+
+            if (num % 3 == 0)
             {
                 return "Fizz";
             }
-            if (dividedBy5(num))
+            if (num % 5 == 0)
             {
                 return "Buzz";
             }
-
             return num.ToString();
         }
-
-        public Boolean dividedBy3(int num)
-        {
-            return num % 3 == 0;
-        }
-        public Boolean dividedBy5(int num)
-        {
-            return num % 5 == 0;
-        }
-
+        */
     }
 }
--- FizzBuzzTest/FizzBuzzTests.cs
+++ FizzBuzzTest/FizzBuzzTests.cs
@@ -7,6 +7,7 @@ namespace FizzBuzz.Tests
 {
     public class FizzBuzzTests
     {
+        /*
         [Theory]
         //[InlineData(new int[] { 1 })]
         [InlineData(new int[] { 1, 2, 3, 5, 6, 10, 15, 30,31 })]
@@ -32,7 +33,29 @@ namespace FizzBuzz.Tests
         }
 
 
-            
+          */
+        
+        [Fact]
+      
+        public void ShouldProcess()
+        {
+            FizzBuzzClass fbObj = new FizzBuzzClass();
+            fbObj.Process(7).Should().Be("7");
+            fbObj.Process(3).Should().Be("Fizz");
+            fbObj.Process(5).Should().Be("Buzz");
+            fbObj.Process(30).Should().Be("FizzBuzz");
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 4, 6, 7, 19, 10, 12, 15, 30, 31 })]
+        public void ShouldExecute(int[] data)
+        {
+            FizzBuzzClass fbObj = new FizzBuzzClass();
+            fbObj.Execute(data).Should().Be("1,2,4,Fizz,7,19,Buzz,Fizz,FizzBuzz,FizzBuzz,31");
+        }
+
+
+
           
 
     }
